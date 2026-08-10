0ed7364039c02772ed8a7c96d2ceb36ec7d36236
mattgibson333 <mattgibson333@gmail.com>
2026-08-10 13:59:10 -0400

    add BSON tag handling and collection support in tests

--- codegen/cruder_test.go
+++ codegen/cruder_test.go
@@ -67,3 +67,75 @@ func TestCrudItem(t *testing.T) {
 	f := c.Entries[0].fields[4]
 	assert.Equal(t, "[]string", f.GoType())
 }
+
+func TestBsonTagsAndCollection(t *testing.T) {
+	mstr := `module x {
+		prefix "x";
+
+		extension collection {
+			argument "name";
+		}
+
+		extension scopes {
+			argument "value";
+		}
+
+		extension col {
+			argument "name";
+		}
+
+		list x {
+			leaf y {
+				type string;
+			}
+			leaf-list z {
+				type int32;
+			}
+			leaf hidden {
+				type string;
+				x:scopes "create,update";
+			}
+			leaf renamed {
+				type string;
+				x:col "legacy_name";
+			}
+		}
+		list withCollection {
+			x:collection "mycoll";
+			leaf id {
+				type string;
+			}
+		}
+	}
+	`
+	m, err := parser.LoadModuleFromString(nil, mstr)
+	require.NoError(t, err)
+	opts := CrudOptions{
+		Entries: []CrudOptionsEntry{
+			{Table: "x", Ydef: "x"},
+			{Table: "withCollection", Ydef: "withCollection"},
+		},
+	}
+	c := NewCruder(opts)
+	require.NoError(t, c.read(m))
+
+	y := c.Entries[0].fields[0]
+	assert.Equal(t, y.JsonTags("read"), y.BsonTags("read"))
+	assert.Equal(t, "y", y.BsonTags("read"))
+
+	z := c.Entries[0].fields[1]
+	assert.Equal(t, "z,omitempty", z.BsonTags("create"))
+
+	hidden := c.Entries[0].fields[2]
+	assert.Equal(t, "-", hidden.BsonTags("read"))
+
+	renamed := c.Entries[0].fields[3]
+	assert.Equal(t, "legacy_name", renamed.BsonTags("read"), "x:col override should apply to the bson tag too, not just gorm's")
+	assert.Equal(t, "renamed", renamed.JsonTags("read"), "the json tag should be unaffected by x:col")
+
+	assert.False(t, c.Entries[0].HasCollection())
+	assert.Equal(t, "", c.Entries[0].Collection())
+
+	assert.True(t, c.Entries[1].HasCollection())
+	assert.Equal(t, "mycoll", c.Entries[1].Collection())
+}
